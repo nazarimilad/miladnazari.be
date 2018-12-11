603c83698d636d8ecb32b34441e81d6e821d746e
Milad Nazari <contact@miladnazari.be>
2018-12-11 14:30:22 +0100

    Correct 404 page

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,10 @@
+#!/usr/bin/env bash
+
 hugo
 /usr/bin/minify -r public/ --url 'text/html' -o public/
 
 git add .
-git commit -m $1
+git commit -m "$1"
 git push
 
 ssh root@miladnazari.be 'cd /var/www/miladnazari.be; git pull'
