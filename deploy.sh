213fd12a22bd60da6328ade699f10c443e8ef62c
Milad Nazari <contact@miladnazari.be>
2018-12-11 14:46:14 +0100

    comment deployement script

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+# generate and render web pages
 hugo
+# minify every html page
 /usr/bin/minify -r public/ --url 'text/html' -o public/
 
+# sync with git repository and hosting server
 git add .
 git commit -m "$1"
 git push
-
 ssh root@miladnazari.be 'cd /var/www/miladnazari.be; git pull'
