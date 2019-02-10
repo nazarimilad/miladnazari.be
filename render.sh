fb5aabfba5274657e7558b373343c515e893a25d
Milad Nazari <contact@miladnazari.be>
2019-02-10 20:04:03 +0100

    Remove unnecessary files generated during PDF build

--- render.sh
+++ render.sh
@@ -7,5 +7,8 @@ for i in "${LANGUAGES[@]}"
 do
     cp public/$i/resume/index.html public/$i/resume/cv-$i.tex
     pdflatex -output-directory=public/$i/resume/ public/$i/resume/cv-$i.tex output-format=pdf
+    rm public/$i/resume/*.aux
+    rm public/$i/resume/*.log
+    rm public/$i/resume/*.out
 done
 
